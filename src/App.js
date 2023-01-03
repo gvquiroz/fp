f48c821512012b91057b9142758953fb88761574
Gabriel Quiroz <gabriel.quiroz@konghq.com>
2023-01-03 17:16:46 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react'
+
+function App() {
+  const {
+    isLoading,
+    error,
+    data,
+  } = useVisitorData();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>An error occurred: {error.message}</div>;
+  }
+
+  if (data) {
+    // perform some logic based on the visitor data
+    return (
+      <div>
+        Welcome {data.visitorFound ? `back ${data.visitorId}  ` : ''}!
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default App;
